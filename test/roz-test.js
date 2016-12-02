3a87f3035715eb5a0de3844cfdf2f6c1a747a223
Bret Doucette <doucette.bret@gmail.com>
2016-12-02 16:40:10 -0700

    Added more tests

--- test/roz-test.js
+++ test/roz-test.js
@@ -24,11 +24,33 @@ describe('ROZ', function () {
     assert.isDefined(this.roz.settings, 'settings have been defined');
   });
 
+  it('should check if it is a chat message', function () {
+    let isMessage = this.roz.isChatMessage(this.fixtures.message);
+    assert.equal(isMessage, true);
+  });
+
+  it('should check to see if the message is coming from Roz', function () {
+    this.roz.user = this.fixtures.userRoz
+    let isFromRoz = this.roz.isFromRozBot(this.fixtures.messageFromRoz);
+    assert.equal(isFromRoz, true);
+  });
+
+  it('should check if the conversation is occurring in a channel', function () {
+    let isMessage = this.roz.isChannelConversation(this.fixtures.message);
+    assert.equal(isMessage, true);
+  });
+
   it('should check if a message contains an acronym and return definition', function () {
     let acronymDefinitionOne = this.roz.checkAndDefineAcronym(this.fixtures.textOne);
     assert.equal(acronymDefinitionOne, 'Cost per click');
 
     let acronymDefinitionTwo = this.roz.checkAndDefineAcronym(this.fixtures.textTwo);
     assert.equal(acronymDefinitionTwo, 'Too long, didn’t read');
+  });
+
+  it('should use a channel id to return a specific channel id', function () {
+    this.roz.channels = this.fixtures.channels
+    let channel = this.roz.getChannelById('C13FS65L7');
+    assert.equal(channel.id, 'C13FS65L7');
   });
 })
